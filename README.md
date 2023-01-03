70383ebb39528526fb251c5bde896df16485a7e1
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2023-01-02 20:25:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ICS2O-PWA-Template-HTML
+
+[![Mr Coxall's Super Linter](https://github.com/Ali-Mugamai/ICS2O-PWA-Test/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Ali-Mugamai/ICS2O-PWA-Test/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/Ali-Mugamai/ICS2O-PWA-Test)](https://repl.it/github/Ali-Mugamai/ICS2O-PWA-Test)
+
+This site can be found at: [https://Ali-Mugamai.github.io/ICS2O-PWA-Test/](https://Ali-Mugamai.github.io/ICS2O-PWA-Test/)
---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2020 ali-mugamai All rights reserved
+//
+// Created by: ali mugamai
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
