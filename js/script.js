de396fdaa168b53abaa3fee09ee148bd0e389494
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2023-01-07 19:56:47 -0500

    uakdhuk

--- js/script.js
+++ js/script.js
@@ -10,15 +10,14 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/personal_project/sw.js", {
+    scope: "/personal_project/",
   })
 }
 
-var img =
-  /**
-   * This function displays an alert.
-   */
-  function myButtonClicked() {
-    document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
-  }
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p> amouna is cringe</p>"
+}
